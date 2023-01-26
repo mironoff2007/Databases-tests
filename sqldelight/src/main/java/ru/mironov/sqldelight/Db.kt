807902e589b4180db9bff709aa8695d89cf83434
Mironov Ury <mironoff2007@rambler.ru>
2023-01-26 20:04:01 +0300

    add plugin, DatabaseDriverFactory

---
+++ sqldelight/src/main/java/ru/mironov/sqldelight/Db.kt
@@ -0,0 +1,11 @@
+package ru.mironov.sqldelight
+
+import android.content.Context
+import com.squareup.sqldelight.android.AndroidSqliteDriver
+import com.squareup.sqldelight.db.SqlDriver
+
+class DatabaseDriverFactory(private val context: Context) {
+    fun createDriver(): SqlDriver {
+        return AndroidSqliteDriver(AppDatabase.Schema, context, "sqldelight_test.db")
+    }
+}
