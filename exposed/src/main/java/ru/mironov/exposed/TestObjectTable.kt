b50a465f07c03dd8b495434c1bec9e32195fc4a1
Mironov Ury <mironoff2007@rambler.ru>
2023-01-30 00:29:07 +0300

    add insert method impl, remove id

--- exposed/src/main/java/ru/mironov/exposed/TestObjectTable.kt
+++ exposed/src/main/java/ru/mironov/exposed/TestObjectTable.kt
@@ -1,23 +1,21 @@
 package ru.mironov.exposed
 
-import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import ru.mironov.domain.BaseTestDTO
-import java.util.*
 
 object TestObjectTable : Table() {
 
-    private val id: Column<EntityID<UUID>> = uuid("id")
-        .autoGenerate()
-        .entityId()
-    override val primaryKey by lazy { super.primaryKey ?: PrimaryKey(id) }
     private val name = TestObjectTable.varchar(name = "name", length = 150)
     private val date = TestObjectTable.varchar(name = "date", length = 50)
     private var foreignId = TestObjectTable.integer(name = "foreignId")
 
-    fun insert(testObject: BaseTestDTO) {
-
+    fun insert(obj: BaseTestDTO) {
+        TestObjectTable.insert {
+            it[name] = obj.name
+            it[date] = obj.date
+            it[foreignId] = obj.foreignId
+        }
     }
 
     fun insertAllBatch(testObjects: List<BaseTestDTO>) {
@@ -61,7 +59,7 @@ object TestObjectTable : Table() {
                             name = it[name],
                             date = it[date],
                             foreignId = it[foreignId],
-                        ).apply { this.id = id }
+                        )//.apply { this.id = it. }
                     }
             }
         } catch (e: Exception) {
